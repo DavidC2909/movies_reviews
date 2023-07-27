dd02fcda0aa4452f06ba3c9d8160e2c741689fc3
david <davidcostea09@gmail.com>
2023-07-27 23:34:15 +0300

    Connected project to MongoDB using MongoDB Compass

---
+++ java/dev/david/movies/MovieService.java
@@ -0,0 +1,13 @@
+package dev.david.movies;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MovieService {
+    private MovieRepository movieRepository; //video ora 1:01:10
+    public List<Movie> AllMovies(){
+
+    }
+}
